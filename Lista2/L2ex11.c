e806287b8e44d6b7de87713a23157cc8251a6008
iglison <iglison@hotmail.com>
2014-05-03 23:13:08 -0300

    Lista 2
    Exercício 11 da Lista 2

--- Lista2/L2ex11.c
+++ Lista2/L2ex11.c
@@ -1,3 +1,24 @@
 /* Tempo de serviço X idade X aposentadoria */
 
 #include <stdio.h>
+
+int main(){
+	int idade, tempo;
+	
+	printf("Digite a idade do trabalhador: ");
+	scanf("%d",&idade);
+	printf("Digite o tempo de servi%co: ",-121);
+	scanf("%d",&tempo);
+	
+	if(idade >= 60 && tempo >= 25){
+		printf("PODE SER APOSENTADO!\n\n");
+	}else{
+		if(idade >= 65 || tempo >= 35){
+			printf("PODE SER APOSENTADO!\n\n");
+		}
+		else{
+			printf("NAO PODE SER APOSENTADO!\n\n");
+		}
+	}
+	return(0);
+}
